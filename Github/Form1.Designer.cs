b85d64f01d6f640044cf807fc7615760e905fb02
Allison <allisonandreavaldiviezoturcio@gmail.com>
2025-09-24 00:01:49 -0500

    llorar

--- Github/Form1.Designer.cs
+++ Github/Form1.Designer.cs
@@ -32,13 +32,16 @@
             button2 = new Button();
             button3 = new Button();
             button4 = new Button();
+            pictureBox1 = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
             SuspendLayout();
             // 
             // button1
             // 
-            button1.Location = new Point(156, 140);
+            button1.Location = new Point(136, 105);
+            button1.Margin = new Padding(3, 2, 3, 2);
             button1.Name = "button1";
-            button1.Size = new Size(94, 29);
+            button1.Size = new Size(82, 22);
             button1.TabIndex = 0;
             button1.Text = "Salir";
             button1.UseVisualStyleBackColor = true;
@@ -46,9 +49,10 @@
             // 
             // button2
             // 
-            button2.Location = new Point(156, 249);
+            button2.Location = new Point(136, 187);
+            button2.Margin = new Padding(3, 2, 3, 2);
             button2.Name = "button2";
-            button2.Size = new Size(94, 29);
+            button2.Size = new Size(82, 22);
             button2.TabIndex = 1;
             button2.Text = "Limpiar";
             button2.UseVisualStyleBackColor = true;
@@ -56,9 +60,10 @@
             // 
             // button3
             // 
-            button3.Location = new Point(156, 196);
+            button3.Location = new Point(136, 147);
+            button3.Margin = new Padding(3, 2, 3, 2);
             button3.Name = "button3";
-            button3.Size = new Size(94, 29);
+            button3.Size = new Size(82, 22);
             button3.TabIndex = 2;
             button3.Text = "xd";
             button3.UseVisualStyleBackColor = true;
@@ -66,24 +71,38 @@
             // 
             // button4
             // 
-            button4.Location = new Point(399, 153);
+            button4.Location = new Point(349, 115);
+            button4.Margin = new Padding(3, 2, 3, 2);
             button4.Name = "button4";
-            button4.Size = new Size(94, 29);
+            button4.Size = new Size(82, 22);
             button4.TabIndex = 3;
             button4.Text = "hola";
             button4.UseVisualStyleBackColor = true;
             // 
+            // pictureBox1
+            // 
+            pictureBox1.Image = Properties.Resources.af29856f91203d1b247498031991702e;
+            pictureBox1.Location = new Point(305, 159);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(269, 149);
+            pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            pictureBox1.TabIndex = 4;
+            pictureBox1.TabStop = false;
+            // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(1006, 721);
+            ClientSize = new Size(880, 541);
+            Controls.Add(pictureBox1);
             Controls.Add(button4);
             Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
             ResumeLayout(false);
         }
 
@@ -93,5 +112,6 @@
         private Button button2;
         private Button button3;
         private Button button4;
+        private PictureBox pictureBox1;
     }
 }
